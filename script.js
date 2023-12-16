d8c5d4d513426908875c6a97cdad58800b037109
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-12-16 21:34:59 +0530

    Implement Responsive Navbar
    Updated the navigation bar to be responsive across various screen sizes. Employed media queries and flexible design elements to ensure optimal user experience on different devices. Tested and confirmed functionality on desktops, tablets, and mobile devices.

---
+++ script.js
@@ -0,0 +1,6 @@
+const navIcon = document.getElementById("navIcon")
+const navOptions  = document.getElementById("navOptions")
+navIcon.addEventListener("click", () => {
+        navOptions.style.display = navOptions.style.display === "block" ? "none" : "block";
+        navIcon.innerHTML = (navOptions.style.display === "block") ? "&#10006;" : "&#9776;";
+    });
