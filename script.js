56cc962c7a2ef544988762d57d112f885a7bd25d
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-12-17 11:32:28 +0530

    Add Banner Section
    Implemented the banner image, banner text and an action button. The banner images seemed to be visually appealing.

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ const navOptions  = document.getElementById("navOptions")
 navIcon.addEventListener("click", () => {
         navOptions.style.display = navOptions.style.display === "block" ? "none" : "block";
         navIcon.innerHTML = (navOptions.style.display === "block") ? "&#10006;" : "&#9776;";
-    });
+});
